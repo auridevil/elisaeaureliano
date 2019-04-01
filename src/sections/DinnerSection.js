baa27b51690ddf2f60d6616323889b61215800a1
auridevil <onailerua@hotmail.com>
2019-04-01 22:40:28 +0200

    commit test

--- src/sections/DinnerSection.js
+++ src/sections/DinnerSection.js
@@ -7,10 +7,10 @@ export class DinnerSection extends React.Component {
   render () {
     return (
       <Section backgroundColor={thirdColor} index={0}>
-        <div>(IMG)</div>
         <div>{'Rinfresco'}</div>
         <div>{'A Palazzo Drago'}</div>
         <div>{'Via Marconi Bla, Verzuolo (CN)'}</div>
+        <div>{'Mappa Qui'}</div>
       </Section>
     )
   }
